17e9d54ce037a5f1b3b25c3e0852ed38d888e736
Mike Wong <wongmdesign@gmail.com>
2019-05-30 17:34:57 -0400

    added remaining tests in case of decremetning below 0

--- src/App.js
+++ src/App.js
@@ -3,21 +3,47 @@ import './App.css'
 
 export default class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
+    error: false
   }
 
   increment = () => {
-    this.setState({ counter: this.state.counter + 1 })
+    if (this.state.error) {
+      this.setState({ error: false })
+    } else {
+      this.setState(prevState => {
+        return { counter: prevState.counter + 1 }
+      })
+    }
+  }
+
+  decrement = () => {
+    if (this.state.counter === 0) {
+      this.setState({ error: true })
+    } else {
+      this.setState(prevState => {
+        return { counter: prevState.counter - 1 }
+      })
+    }
   }
 
   render () {
+    const { counter, error } = this.state
+    const errorClass = error ? '' : 'hidden'
     return (
       <div data-test='component-app'>
         <h1 data-test='counter-display'>
           The counter is currently {this.state.counter}
         </h1>
+        <div data-test='error-message' className={`error ${errorClass}`}>
+          The counter cannot go below 0
+        </div>
+
         <button data-test='increment-button' onClick={this.increment}>
           Increment counter
+        </button>
+        <button data-test='decrement-button' onClick={this.decrement}>
+          Decrement counter
         </button>
       </div>
     )
